020e21b7758b67b53b4ec88257fe76c44a4870e4
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-12-18 17:13:42 +1100

    starting project

--- server.c
+++ server.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
-int main(void)
+//in this project i should bind to port 127.0.0.1 (local host) and NOT 0.0.0.0
+//run server on port above 1024, stick to port 8080
+
+
+//link with ws2_32.lib
+//dunno what this means but whatever
+#pragma comment(lib, "ws2_32.lib")
+
+#define port 8080
+#define BUFFER_SIZE 1024
+
+int main()
 {
-    printf("%s\n","Hello, World");   
-    char *yes = "yes";
-    printf("%s\n", yes);
+    WSADATA wsadata;
+    SOCKET serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    int addrLen = sizeof(clientAddr);
+    char buffer[BUFFER_SIZE];
+    
 }
